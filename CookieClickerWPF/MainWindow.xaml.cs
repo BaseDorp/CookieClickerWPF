419a709a4b95a7f05a79de75c00809b6d2d0a34a
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2018-11-03 02:07:28 -0500

    - You now earn cookies per second - Added very simple and broken upgrade system - Tried to make so that the cookies variable would round to 4 deicmals places but it doesn't work yet

--- CookieClickerWPF/MainWindow.xaml.cs
+++ CookieClickerWPF/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+
 namespace CookieClickerWPF
 {
     /// <summary>
@@ -20,18 +22,75 @@ namespace CookieClickerWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Global Variables
         double Cookies = 0;
+        double UpgradeCost = 10;
+        double CPS = 1;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            // creates instance 
+            DispatcherTimer TickTimer = new System.Windows.Threading.DispatcherTimer();
+            TickTimer.Tick += new EventHandler(Timer);
+            // Sets time interval on how often TickTimer is called
+            TickTimer.Interval = new TimeSpan(0, 0, 1);
+            // Starts the timer
+            TickTimer.Start();
         }
 
+        void Timer(object sender, EventArgs e)
+        {
+            Cookies = Cookies + CPS;
+            txt_Cookies.Text = Convert.ToString(Cookies);
+        }
+
+        // Adds the CPS to the total amount of Cookies every time the button is clicked
         private void bt_Clicker_Click(object sender, RoutedEventArgs e)
         {
-            Cookies++;
-
+            Cookies = Cookies + CPS;
+            
+            Cookies = Round(Cookies);
             txt_Cookies.Text = Convert.ToString(Cookies);
+        }
+
+        private void bt_Multiplyer_Click(object sender, RoutedEventArgs e)
+        {
+            // Player needs to have enough cookies to meet the UpgradeCost
+            if (UpgradeCost <= Cookies)
+            {
+                // Takes away the Cookies for the upgrade
+                Cookies = Cookies - UpgradeCost;
+                // Sets a new upgrade cost
+                UpgradeCost = UpgradeCost * 1.6;
+                // Changes how many cookies per click you get
+                CPS = CPS * 1.4;
+                // Updates the Cookie count to the new deducted amount
+                txt_Cookies.Text = Convert.ToString(Cookies);
+            }
+            // If player doesn't have enough cookies tell them
+            else
+            {
+                txt_Multiplier.Text = "Not Enough Cookies";
+            }
+            // Either way update what the button says the cost is to the new cost
+            bt_Multiplyer.Content = "Cost : " + UpgradeCost;
+
+            Cookies = Round(Cookies);
+            txt_Cookies.Text = Convert.ToString(Cookies);
+        }
+
+        double Round (double _roundy)
+        {
+            // Code from RodH257 at https://stackoverflow.com/questions/6092243/c-sharp-check-if-a-decimal-has-more-than-3-decimal-places
+            // Checks if the number has 4 decimal places is equal to the number
+            if (Decimal.Round(Convert.ToDecimal(_roundy), 4) != Convert.ToDecimal(_roundy))
+            {
+                Decimal.Round(Convert.ToDecimal(_roundy), 4);
+                return Convert.ToDouble(_roundy);
+            }
+            return Convert.ToDouble(_roundy);
         }
     }
 }

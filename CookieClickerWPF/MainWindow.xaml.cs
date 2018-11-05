77354e866f53688dd33a143eea5a7e57ab291099
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2018-11-05 01:36:40 -0600

    - Added cookie button image - Added 3 different upgrades - The "not enough cookies" text now changes back to "Upgrades:" after 3 seconds - The player now gets cookies per second + Want to remove that when you hover over the button, it doesn;t highlight and cover the image + Rounding still doesn't work perfectly

--- CookieClickerWPF/MainWindow.xaml.cs
+++ CookieClickerWPF/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Shapes;
 
 using System.Windows.Threading;
 
+// Task.Delay code from Jimmy Hannon at https://stackoverflow.com/questions/24371440/how-to-run-a-method-after-a-specific-time-interval
+// Decimal.Round code from RodH257 at https://stackoverflow.com/questions/6092243/c-sharp-check-if-a-decimal-has-more-than-3-decimal-places
+
 namespace CookieClickerWPF
 {
     /// <summary>
@@ -25,6 +28,8 @@ namespace CookieClickerWPF
         // Global Variables
         double Cookies = 0;
         double UpgradeCost = 10;
+        double GrandmaUpCost = 25;
+        double FactoryUpCost = 100;
         double CPS = 1;
 
         public MainWindow()
@@ -33,14 +38,17 @@ namespace CookieClickerWPF
 
             // creates instance 
             DispatcherTimer TickTimer = new System.Windows.Threading.DispatcherTimer();
-            TickTimer.Tick += new EventHandler(Timer);
+            TickTimer.Tick += new EventHandler(TicTimer);
+
             // Sets time interval on how often TickTimer is called
             TickTimer.Interval = new TimeSpan(0, 0, 1);
+
             // Starts the timer
             TickTimer.Start();
         }
-
-        void Timer(object sender, EventArgs e)
+        
+        // Adds the CPS every time called
+        void TicTimer(object sender, EventArgs e)
         {
             Cookies = Cookies + CPS;
             txt_Cookies.Text = Convert.ToString(Cookies);
@@ -55,7 +63,8 @@ namespace CookieClickerWPF
             txt_Cookies.Text = Convert.ToString(Cookies);
         }
 
-        private void bt_Multiplyer_Click(object sender, RoutedEventArgs e)
+        // Function must be async so the await works
+        private async void bt_Upgrade_Click(object sender, RoutedEventArgs e)
         {
             // Player needs to have enough cookies to meet the UpgradeCost
             if (UpgradeCost <= Cookies)
@@ -63,27 +72,74 @@ namespace CookieClickerWPF
                 // Takes away the Cookies for the upgrade
                 Cookies = Cookies - UpgradeCost;
                 // Sets a new upgrade cost
-                UpgradeCost = UpgradeCost * 1.6;
+                UpgradeCost = UpgradeCost * 5;
                 // Changes how many cookies per click you get
-                CPS = CPS * 1.4;
+                CPS = CPS * 2;
                 // Updates the Cookie count to the new deducted amount
                 txt_Cookies.Text = Convert.ToString(Cookies);
             }
-            // If player doesn't have enough cookies tell them
             else
             {
-                txt_Multiplier.Text = "Not Enough Cookies";
+                txt_Upgrades.Text = "Not Enough Cookies";
+                // waits for 2 seconds before moving on
+                await Task.Delay(2000);
+                txt_Upgrades.Text = "Upgrades:";
             }
             // Either way update what the button says the cost is to the new cost
-            bt_Multiplyer.Content = "Cost : " + UpgradeCost;
+            bt_Upgrade.Content = "CPS * 2\n\nCost : " + UpgradeCost;
 
             Cookies = Round(Cookies);
             txt_Cookies.Text = Convert.ToString(Cookies);
+            txt_CPS.Text = "Cookies per second: " + CPS;
+        }
+        
+        private  async void bt_Grandma_Click(object sender, RoutedEventArgs e)
+        {
+            if (GrandmaUpCost <= Cookies)
+            {
+                Cookies = Cookies - GrandmaUpCost;
+                GrandmaUpCost = GrandmaUpCost * 1.5;
+                CPS = CPS * 0.2;
+                txt_Cookies.Text = Convert.ToString(Cookies);
+            }
+            else
+            {
+                txt_Upgrades.Text = "Not Enough Cookies";
+                await Task.Delay(2000);
+                txt_Upgrades.Text = "Upgrades:";
+            }
+            bt_Grandma.Content = "CPS * 0.2\n\nCost : " + GrandmaUpCost;
+
+            Cookies = Round(Cookies);
+            txt_Cookies.Text = Convert.ToString(Cookies);
+            txt_CPS.Text = "Cookies per second: " + CPS;
         }
 
-        double Round (double _roundy)
+        private async void bt_Factory_Click(object sender, RoutedEventArgs e)
         {
-            // Code from RodH257 at https://stackoverflow.com/questions/6092243/c-sharp-check-if-a-decimal-has-more-than-3-decimal-places
+            if (FactoryUpCost <= Cookies)
+            {
+                Cookies = Cookies - FactoryUpCost;
+                FactoryUpCost = FactoryUpCost * 3;
+                CPS = CPS * .5;
+                txt_Cookies.Text = Convert.ToString(Cookies);
+            }
+            else
+            {
+                txt_Upgrades.Text = "Not Enough Cookies";
+                await Task.Delay(2000);
+                txt_Upgrades.Text = "Upgrades:";
+            }
+            bt_Factory.Content = "CPS * 0.5\n\nCost : " + FactoryUpCost;
+
+            Cookies = Round(Cookies);
+            txt_Cookies.Text = Convert.ToString(Cookies);
+            txt_CPS.Text = "Cookies per second: " + CPS;
+        }
+
+        double Round(double _roundy)
+        {
+
             // Checks if the number has 4 decimal places is equal to the number
             if (Decimal.Round(Convert.ToDecimal(_roundy), 4) != Convert.ToDecimal(_roundy))
             {

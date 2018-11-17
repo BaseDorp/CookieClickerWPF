ec348465ee3ae5c720e0888a7ec9730a5d6cccda
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2018-11-17 03:07:38 -0600

    - Now rounding using Math.Floor which rounds to the nearest integer + I would like to change it to round to 2 deicmal places instead

--- CookieClickerWPF/MainWindow.xaml.cs
+++ CookieClickerWPF/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 
 // Task.Delay code from Jimmy Hannon at https://stackoverflow.com/questions/24371440/how-to-run-a-method-after-a-specific-time-interval
-// Decimal.Round code from RodH257 at https://stackoverflow.com/questions/6092243/c-sharp-check-if-a-decimal-has-more-than-3-decimal-places
+// Math.Floor code from https://www.dotnetperls.com/math-floor
 
 namespace CookieClickerWPF
 {
@@ -51,8 +51,8 @@ namespace CookieClickerWPF
         void TicTimer(object sender, EventArgs e)
         {
             Cookies = Cookies + CPS;
-            // Cookies = Round(Cookies);
-            // CPS = Round(CPS);
+            Cookies = Math.Floor(Cookies);
+            CPS = Math.Floor(CPS);
             txt_Cookies.Text = Convert.ToString(Cookies);
         }
 
@@ -86,7 +86,8 @@ namespace CookieClickerWPF
                 await Task.Delay(2000);
                 txt_Upgrades.Text = "Upgrades:";
             }
-            // Either way update what the button says the cost is to the new cost
+            // Either way update what the button says the cost is to the new cost\
+            UpgradeCost = Math.Floor(UpgradeCost);
             bt_Upgrade.Content = "CPS * 2\n\nCost : " + UpgradeCost;
 
             txt_Cookies.Text = Convert.ToString(Cookies);
@@ -108,6 +109,7 @@ namespace CookieClickerWPF
                 await Task.Delay(2000);
                 txt_Upgrades.Text = "Upgrades:";
             }
+            GrandmaUpCost = Math.Floor(GrandmaUpCost);
             bt_Grandma.Content = "CPS * 0.2\n\nCost : " + GrandmaUpCost;
             
             txt_Cookies.Text = Convert.ToString(Cookies);
@@ -129,22 +131,11 @@ namespace CookieClickerWPF
                 await Task.Delay(2000);
                 txt_Upgrades.Text = "Upgrades:";
             }
+            FactoryUpCost = Math.Floor(FactoryUpCost);
             bt_Factory.Content = "CPS * 0.5\n\nCost : " + FactoryUpCost;
             
             txt_Cookies.Text = Convert.ToString(Cookies);
             txt_CPS.Text = "Cookies per second: " + CPS;
-        }
-
-        double Round(double _roundy)
-        {
-
-            // Checks if the number has 4 decimal places is equal to the number
-            if (Decimal.Round(Convert.ToDecimal(_roundy), 4) != Convert.ToDecimal(_roundy))
-            {
-                Decimal.Round(Convert.ToDecimal(_roundy), 4);
-                return Convert.ToDouble(_roundy);
-            }
-            return Convert.ToDouble(_roundy);
         }
     }
 }
